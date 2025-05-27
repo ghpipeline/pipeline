5827f0af711e9e82582a16474efb1c6516c87fd4
dustinlegacy <dustinh@legacyg.com>
2025-05-26 18:40:10 -0700

    adding dataset

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,10 +42,50 @@ resource "google_storage_bucket" "my_bucket" {
   force_destroy = true
 }
 
-#adding a subfolder
+## adding a subfolder ##
 
 resource "google_storage_bucket_object" "subfolder" {
   name   = "gdp_data/"
   bucket = "world_bank_raw"
   content = "/dev/null" # creates an empty object that looks like a folder
+}
+
+## to set ups big querey we must first make a data set to put taables into ##
+
+resource "google_bigquery_dataset" "world_bank_dataset" {
+  dataset_id                  = "world_bank_data"
+  friendly_name               = "World Bank Data"
+  location                    = "US"
+  delete_contents_on_destroy = true
+}
+
+
+##making the table in big querey to put data into the dataset we created##
+
+resource "google_bigquery_table" "gdp_table" {
+  dataset_id = google_bigquery_dataset.world_bank_dataset.dataset_id
+  table_id   = "gdp_2022"
+  project    = "globalhealthdatascience"
+
+  schema = <<EOF
+[
+  {
+    "name": "country",
+    "type": "STRING",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "year",
+    "type": "INTEGER",
+    "mode": "REQUIRED"
+  },
+  {
+    "name": "gdp",
+    "type": "FLOAT",
+    "mode": "NULLABLE"
+  }
+]
+EOF
+
+  deletion_protection = false
 }
