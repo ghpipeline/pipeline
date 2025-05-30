f482d0bf216b0e8177565a644453653319fa6f92
Dustin Hoye <160974279+dustinlegacy@users.noreply.github.com>
2025-05-30 00:42:35 -0700

    Update gdp.py

--- deploy/gdp.py
+++ deploy/gdp.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
             url="https://github.com/rookcap/prefect-legacy.git",
             credentials={"access_token": Secret.load("git-hub-access-token")}
         ),
-        entrypoint="scripts/datapull.py:CORE_VALUES",
+        entrypoint="scripts/prefectdatapull.py:gdp_pipeline",
     ).deploy(
-        name="CORE-VALUES",
+        name="CORE-gdp_pipeline",
         work_pool_name="gcp-cloud-run"
     )
