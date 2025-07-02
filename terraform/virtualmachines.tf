721180e52b526d0542b5d45d50b59bbadd3fe491
ghpipeline <globalhealthdatascience@gmail.com>
2025-07-02 11:56:13 -0700

    Update virtualmachines.tf

--- terraform/virtualmachines.tf
+++ terraform/virtualmachines.tf
@@ -1,8 +1,9 @@
-##adding a VM for running cloud jobs## 
+## Adding a VM for running cloud jobs ##
 
 resource "google_compute_instance" "vm_instance" {
   name         = "terraform-instance"
   machine_type = "e2-medium"
+  zone         = "us-central1-a" # Add zone if not already in your provider block
 
   allow_stopping_for_update = true
 
@@ -13,14 +14,33 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    # A default network is created for all GCP projects
     network = "default"
-    access_config {
-    }
+    access_config {}
   }
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+
+    cd /home/globalhealthdatascience/airflow-project || exit 1
+
+    # Clean up old DAGs
+    rm -rf dags/*
+
+    # Clone latest from GitHub
+    git clone https://github.com/ghpipeline/pipeline.git temp
+
+    # Copy DAGs from repo to Airflow
+    cp -r temp/DAGs/* dags/
+
+    # Clean up
+    rm -rf temp
+
+    # Launch Airflow
+    docker compose up airflow-init && docker compose up -d
+  EOT
 }
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
+  name                     = "terraform-network"
+  auto_create_subnetworks = true
 }
