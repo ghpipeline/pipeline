8cbd0b9968d1f241bad597cbf2177504a09c43dc
ghpipeline <globalhealthdatascience@gmail.com>
2025-07-09 10:57:09 -0700

    Update BQ_CLEAN_TO_ML_PREPED.py

--- DAGs/BQ_CLEAN_TO_ML_PREPED.py
+++ DAGs/BQ_CLEAN_TO_ML_PREPED.py
@@ -17,7 +17,7 @@ ML_PREP_SQL = f"""
 CREATE OR REPLACE TABLE `{ML_PREPED_TABLE}` AS
 SELECT
     *,
-    SAFE_CAST(value_numeric AS FLOAT64) AS value_numeric_float,
+    SAFE_CAST(value AS FLOAT64) AS value_numeric,
     SAFE.PARSE_DATE('%Y%m%d', recall_initiation_date) AS recall_initiation_dt,
     SAFE.PARSE_DATE('%Y%m%d', center_classification_date) AS center_classification_dt,
     SAFE.PARSE_DATE('%Y%m%d', termination_date) AS termination_dt,
@@ -48,7 +48,7 @@ SELECT
     REGEXP_CONTAINS(reason_for_recall, r'(?i)steril') AS mention_sterility,
     CURRENT_TIMESTAMP() AS prepped_at
 FROM `{CLEANED_TABLE}`
-WHERE value_numeric IS NOT NULL
+WHERE SAFE_CAST(value AS FLOAT64) IS NOT NULL
 """
 
 with DAG(
